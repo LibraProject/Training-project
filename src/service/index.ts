6c6cef529b972eae16f985bed8cfcb4d062d2357
ex78 <774421703@qq.com>
2019-09-04 14:43:00 +0800

    ''

--- src/service/index.ts
+++
@@ -1,3 +0,0 @@
-//引入所有 模块 并抛出
-
-export * from './user'
--- src/service/user.ts
+++
@@ -1,7 +0,0 @@
-import request from '../utils/request'
-
-//登录借口
-//params 值为对象 
-export let login = (params:object)=>{
-    return request.post('/user/login',params)
-}
