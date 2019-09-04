505089882b312f59200faaa1f8e41f4f5d5eb46b
LHY <lhy136890@163.com>
2019-09-04 15:21:53 +0800

    fourth

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,7 +1,7 @@
 import { Breadcrumb, Dropdown, Icon, Layout, Menu } from 'antd';
 import * as React from 'react';
 import "../../scss/home.css";
-import { NavLink } from 'react-router-dom'
+import Submenu from '../../components/submenu'
 
 
 const { SubMenu } = Menu;
@@ -52,83 +52,9 @@ class Home extends React.Component {
           </Menu>
         </Header>
         <Layout>
-          <Sider width={200}>
-            <Menu
-              theme="dark"
-              mode="inline"
-              defaultSelectedKeys={['1']}
-              defaultOpenKeys={['sub1']}
-              style={{ height: '100%', borderRight: 0 }}
-            >
-              <SubMenu
-                key="sub1"
-                title={
-                  <span>
-                    <Icon type="sliders" />
-                    试题管理
-                </span>
-                }
-              >
-                <Menu.Item key="1">
-                添加试题
-                  {/* <a href="/home/rank"> 添加试题</a> */}
-                </Menu.Item>
-                <Menu.Item key="2">
-                  <NavLink to="/home/classify">试题分类</NavLink>
-                </Menu.Item>
-                <Menu.Item key="3">查看试题</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub2"
-                title={
-                  <span>
-                    <Icon type="user" />
-                    用户管理
-                </span>
-                }
-              >
-                <Menu.Item key="5">添加用户</Menu.Item>
-                <Menu.Item key="6">用户展示</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub3"
-                title={
-                  <span>
-                    <Icon type="schedule" />
-                    考试管理
-                  </span>
-                }
-              >
 
-                <Menu.Item key="7">添加考试</Menu.Item>
-                <Menu.Item key="8">试卷列表</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub4"
-                title={
-                  <span>
-                    <Icon type="project" />
-                    班级管理
-                  </span>
-                }
-              >
-                <Menu.Item key="9">班级管理</Menu.Item>
-                <Menu.Item key="10">教室管理</Menu.Item>
-                <Menu.Item key="11">学生管理</Menu.Item>
-              </SubMenu>
-              <SubMenu
-                key="sub5"
-                title={
-                  <span>
-                    <Icon type="project" />
-                    阅卷管理
-                  </span>
-                }
-              >
-                <Menu.Item key="12">特批班级</Menu.Item>
-              </SubMenu>
-            </Menu>
-          </Sider>
+          <Submenu/>
+
           <Layout style={{ padding: '0 24px 24px' }}>
             <Breadcrumb style={{ margin: '16px 0' }}>
               <Breadcrumb.Item>Home</Breadcrumb.Item>
