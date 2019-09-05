036697f08331d7304db4d612dd34e9502658811f
ex78 <774421703@qq.com>
2019-09-05 21:40:03 +0800

    '添加试题分类'

--- src/store/modules/question.ts
+++ src/store/modules/question.ts
@@ -1,26 +1,28 @@
-import { observable, action } from 'mobx'
-import { getQuestion, examType, getQuestionsType } from '../../service/index'
+import { action } from 'mobx'
+import { getQuestion, examType, getQuestionsType,addQuestions } from '../../service/index'
 
 class Question {
 
     // 按条件获取试题
     @action async getQuestion(params: any): Promise<any> {
         let result: any = await getQuestion(params);
-        // console.log('question...', result);
         return result.data
     }
     // 考试类型获取
     @action async examType(type: any): Promise<any> {
         const result: any = await examType();
-        console.log('question...', result);
         return result.data
     }
 
     // 题目类型获取
     @action async getQuestionsType(): Promise<any> {
         const result: any = await getQuestionsType();
-        // console.log('question...', result);
         return result.data
+    }
+
+    @action async addQuestions(params: any): Promise<any> {
+        const result: any = await addQuestions(params);
+        return result
     }
 }
 
