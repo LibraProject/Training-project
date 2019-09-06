1c9709d254e070fbc806bd349f82e27ab641bc62
ex78 <774421703@qq.com>
2019-09-06 19:22:54 +0800

    '合并'

--- src/service/question.ts
+++ src/service/question.ts
@@ -1,16 +1,10 @@
 import request from "../utils/requerst";
 
 // 获取试题
-<<<<<<< HEAD
-export let getQuestion = (params:object)=>{
-    // console.log(params)
-    return request.get('/exam/questions/condition',{params})
-}
-=======
 export const getQuestion = (params: object) => {
   return request.get("/exam/questions/condition", { params });
 };
->>>>>>> cyz
+
 
 // 考试类型获取成功
 export const examType = () => {
@@ -23,7 +17,6 @@ export const getQuestionsType = () => {
 };
 
 // 所有的课程获取成功
-<<<<<<< HEAD
 export let subject = ()=>{
     return request.get('/exam/subject')
 }
@@ -37,15 +30,3 @@ export let userInfo = ()=>{
 export let addQuestion = (params:object)=>{
     return request.post('/exam/questions',params)
 }
-=======
-export const subject = () => {
-  return request.get("/exam/subject");
-};
-
-// 添加试题类型
-export const addQuestions = (params: object) => {
-  return request.get("/exam/insertQuestionsType", { params });
-};
-
-
->>>>>>> cyz
