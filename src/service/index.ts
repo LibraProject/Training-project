eb9b142e42bed8171891fe39dc35e5008e88d6f8
ex78 <774421703@qq.com>
2019-09-07 11:24:48 +0800

    't'

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,8 +1,5 @@
 export * from './user'
 export * from './question'
-<<<<<<< HEAD
 export * from './classroom'
-=======
 export * from './exam'
 export * from './manger'
->>>>>>> b797c01dd4ccbd90ea566d10df69a9276ba45f6c
